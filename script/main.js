aaf0d28d406329bd8179438849685e161a67959b
Clouddd1 <152765710+Clouddd1@users.noreply.github.com>
2023-12-04 22:50:53 +0700

    Update main.js

--- script/main.js
+++ script/main.js
@@ -1,21 +1,30 @@
 document.addEventListener('DOMContentLoaded', function () {
     const audio = document.getElementById('myAudio');
-    const maafOption = document.getElementById('maafOption');
+    const yaButton = document.getElementById('yaButton');
+    const tidakButton = document.getElementById('tidakButton');
+    const yaResponse = document.getElementById('yaResponse');
+    const tidakResponse = document.getElementById('tidakResponse');
+    const responseContainer = document.querySelector('.response-container');
     const apologyMessage = document.querySelector('.apology-message');
 
     // Gantilah 'music/heri.mp3' dengan path yang sesuai ke file musik Anda
     audio.src = 'music/heri.mp3';
 
-    // Mulai memutar musik secara otomatis
-    audio.play();
+    // Tambahkan event listener untuk tombol "Ya"
+    yaButton.addEventListener('click', function () {
+        yaResponse.style.display = 'block';
+        tidakResponse.style.display = 'none';
+        apologyMessage.style.display = 'none';
+        responseContainer.style.display = 'block';
+        audio.play(); // Play audio saat tombol "Ya" diklik
+    });
 
-    // Tambahkan event listener untuk menangani pilihan pengguna
-    maafOption.addEventListener('change', function () {
-        const chosenOption = maafOption.value;
-        if (chosenOption === 'ya') {
-            apologyMessage.textContent = 'Terima kasih sudah memaafkanku. Aku sayang kamu ❤️';
-        } else {
-            apologyMessage.textContent = 'Gapapa, aku akan berusaha lebih baik. Aku sayang kamu ❤️';
-        }
+    // Tambahkan event listener untuk tombol "Tidak"
+    tidakButton.addEventListener('click', function () {
+        yaResponse.style.display = 'none';
+        tidakResponse.style.display = 'block';
+        apologyMessage.style.display = 'none';
+        responseContainer.style.display = 'block';
+        audio.play(); // Play audio saat tombol "Tidak" diklik
     });
 });
